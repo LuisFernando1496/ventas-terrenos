1802f09fd6a69b533f3b98f6ee325986f7cd2bb2
LuisFernando1496 <luisfernando_orantes@outlook.com>
2021-10-31 18:28:49 -0600

    items sidebar

--- resources/views/livewire/sidebar-nav-bar.blade.php
+++ resources/views/livewire/sidebar-nav-bar.blade.php
@@ -31,24 +31,47 @@
             <span class="text-sm font-medium">Productos</span>
           </a>
         </li>
-        <li>  
         @endcan
+        <li>  
           <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-chat"></i></span>
-            <span class="text-sm font-medium">Chat</span>
+            <span class="text-sm font-medium">Ver Planos</span>
+          </a>
+        </li>
+        <li>  
+          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-chat"></i></span>
+            <span class="text-sm font-medium">Vender</span>
+          </a>
+        </li>
+        <li>  
+          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-chat"></i></span>
+            <span class="text-sm font-medium">Ventas</span>
+          </a>
+        </li>
+        <li>  
+          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-chat"></i></span>
+            <span class="text-sm font-medium">Terrenos</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-music"></i></span>
-            <span class="text-sm font-medium">Profile</span>
+            <span class="text-sm font-medium">Compras</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-music"></i></span>
+            <span class="text-sm font-medium">Reportes</span>
           </a>
         </li>
         <li>
           <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
             <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-bell"></i></span>
-            <span class="text-sm font-medium">Notifications</span>
-            <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
+            <span class="text-sm font-medium">Devoluciones</span>
           </a>
         </li>
        
