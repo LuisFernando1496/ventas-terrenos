5a211bb028cef12e8fb5b81460cb6020528270ac
LuisFernando1496 <luisfernando_orantes@outlook.com>
2021-11-27 18:55:51 -0600

    detalles de abonos

--- resources/views/clients/historySale.blade.php
+++ resources/views/clients/historySale.blade.php
@@ -33,7 +33,7 @@
                         </thead>
                         <tbody>
                             @forelse ($cliente[0]->sales as $sale)
-                           
+                          
                                 <tr>
                                     
                                     <td>{{$sale->id}}</td>
@@ -53,9 +53,13 @@
                                    @if (($key+1) === $llaves)
                                    <td style="color:white"> 
                                         @if ($itemHistory->faltante > 0)
-                                        <p style="background-color: red; ">Adeudo</p>
+                                            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal{{$sale->id}}"> 
+                                              <p style="background-color: red; ">Adeudo</p>
+                                            </button>
                                         @else
-                                        <p style="background-color: green">Pagado</p>
+                                            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal{{$sale->id}}">
+                                                <p style="background-color: green">Pagado</p>
+                                            </button>>
                                         @endif
                                     </td>
                                     <td>${{$itemHistory->faltante}}</td>
@@ -81,156 +85,11 @@
                           
                              
                                     <td>$ {{$sale->cart_total}}</td>
-                                   
+                               
                                   
                                 </tr>
                                 <!-- Modal -->
-                         {{--    <div class="modal fade" id="exampleModal{{ $cliente->id }}" tabindex="-1"
-                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <form action="{{ route('clients.update', $cliente) }}" method="POST">
-                                                @csrf 
-                                                @method('PATCH')
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Editar cliente
-                                                        {{ $cliente->name }}</h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                               <div class="modal-body">
-                                    <div class="row">
-                                        <input type="hidden" name="address_id" value="{{$cliente->address_id}}">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Nombre</label>
-                                                <input type="text" class="form-control" name="name" value="{{$cliente->name}}" required> 
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Apellidos</label>
-                                                <input type="text" class="form-control" name="last_name" value="{{$cliente->last_name}}" required>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">RFC</label>
-                                                <input type="text" class="form-control" name="rfc" value="{{$cliente->rfc}}" required>
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Correo</label>
-                                                <input type="email" class="form-control" name="email" value="{{$cliente->email}}" required>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Colonia</label>
-                                                <input type="text" class="form-control" name="colonia" value="{{$cliente->direccion->colonia}}" required> 
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Calle</label>
-                                                <input type="text" class="form-control" name="calle" value="{{$cliente->direccion->calle}}" required>
-                                            </div>
-                                        </div>
-                                       
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Telefono</label>
-                                                <input type="text" class="form-control" name="phonenumber" value="{{$cliente->phonenumber}}"  required> 
-                                            </div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Numero Int..</label>
-                                                <input type="text" class="form-control" name="numero_int" value="{{$cliente->direccion->numero_int}}" placeholder="Opcional" required> 
-                                            </div>
-                                        </div>
-                                      
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Numero Ext.</label>
-                                                <input type="text" class="form-control" name="numero_ext" value="{{$cliente->direccion->numero_ext}}" placeholder="Opcional" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Estado</label>
-                                                <select class="form-control" type="text" name="estado" value="{{$cliente->direccion->estado}}" id="estado" value="" required>
-                                                    <option value="Ciudad de México">Ciudad de México</option>
-                                                    <option value="Aguascalientes">Aguascalientes</option>
-                                                    <option value="Baja California">Baja California</option>
-                                                    <option value="Baja California sur">Baja California Sur</option>
-                                                    <option value="Campeche">Campeche</option>
-                                                    <option value="Chiapas">Chiapas</option>
-                                                    <option value="Chihuahua">Chihuahua</option>
-                                                    <option value="Coahuila">Coahuila</option>
-                                                    <option value="Colima">Colima</option>
-                                                    <option value="Durango">Durango</option>
-                                                    <option value="Guanajuato">Guanajuato</option>
-                                                    <option value="Guerrero">Guerrero</option>
-                                                    <option value="Hidalgo">Hidalgo</option>
-                                                    <option value="Jalisco">Jalisco</option>
-                                                    <option value="Cd. México">Cd. México</option>
-                                                    <option value="Michoacán">Michoacán</option>
-                                                    <option value="Morelos">Morelos</option>
-                                                    <option value="Nayarit">Nayarit</option>
-                                                    <option value="Nuevo León">Nuevo León</option>
-                                                    <option value="Oaxaca">Oaxaca</option>
-                                                    <option value="Puebla">Puebla</option>
-                                                    <option value="Querétaro">Querétaro</option>
-                                                    <option value="Quintana Roo">Quintana Roo</option>
-                                                    <option value="San Luis Potosí">San Luis Potosí</option>
-                                                    <option value="Sinaloa">Sinaloa</option>
-                                                    <option value="Sonora">Sonora</option>
-                                                    <option value="Tabasco">Tabasco</option>
-                                                    <option value="Tamaulipas">Tamaulipas</option>
-                                                    <option value="Tlaxcala">Tlaxcala</option>
-                                                    <option value="Veracruz">Veracruz</option>
-                                                    <option value="Yucatán">Yucatán</option>
-                                                    <option value="Zacatecas">Zacatecas</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                   
-                                    <div class="row">
-                                        
-                                        <div class="col">
-                                            <div class="form-group">
-                                                <label for="">Ciudad</label>
-                                                <input type="text" class="form-control" name="ciudad" value="{{$cliente->direccion->ciudad}}"  required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                   
-                                   
-
-                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">Cerrar</button>
-                                                    <button type="submit" class="btn btn-primary">Actualizar</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>--}}
+                           
                             @empty
 
                             @endforelse
@@ -240,6 +99,54 @@
                 </div>
                 </div>
                 </div>
-                <div>  <h3 class="float-right font-semibold text-xl text-gray-800 leading-tight" style="padding-right: 20px">Deuda total: ${{$deuda}}</h3></div>
+                <div>
+                      <h3 class="float-right font-semibold text-xl text-gray-800 leading-tight" style="padding-right: 20px">Deuda total: ${{$deuda}}</h3>
+                    </div>
+                    @forelse ($cliente[0]->sales as $sale)
+                    <div class="modal fade" id="exampleModal{{$sale->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                               
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Historial abonos
+                                            {{ $cliente[0]->name }}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                   <div class="modal-body">
+                                      
+                                    <table class="table table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th>ID Venta</th>
+                                                <th>Abono</th>
+                                                <th>Faltante</th>
+                                                <th>Fecha</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                         @forelse ($sale->abonos as $key => $itemHistory)  
+                                            <tr>
+                                                <td>{{$sale->id}}</td>
+                                                <td>${{$itemHistory->pay}}</td>
+                                                <td>${{$itemHistory->faltante}}</td>
+                                                <td>{{$itemHistory->created_at}}</td>
+                                            </tr>
+                                            @empty
+                                            @endforelse                                              
+                                        </tbody>
+                                    </table>
+                       
+                                     </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                                    </div>
+                           
+                            </div>
+                        </div>
+                    </div>
+          @empty
               
+          @endforelse
+           
 </x-app-layout>
